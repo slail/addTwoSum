49ca36b50397f47175a97b3d65c9e5a75331842f
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-05 20:59:43 -0400

    added two algorithms

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#AddTwo Sum Integers
+# AddTwo Sum Integers
 
 """
 Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum.
@@ -8,3 +8,30 @@ Note that the target sum has to be obtained by summing two different integers in
 you can't add a single integer to itself in order to obtain the target sum.
 You can assume that there will be at most one pair of numbers summing up to the target sum.
 """
+
+
+# Solution 1 - With a hashTable
+def addTwoSum(array, targetSum):
+    hashtable = {}
+    for num in array:
+        y = targetSum - num
+        if y in hashtable:
+            return [y, num]
+        else:
+            hashtable[num] = True
+    return []
+
+# Solution 2 - Left/Right Pointers
+def addTwoSum2(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right: # while left pointer is not passed
+        currentSum = array[left] + array[right]
+        if currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1
+        elif currentSum == targetSum:
+            return [array[left], array[right]]
+    return []
