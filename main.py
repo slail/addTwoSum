e78818c9912cb9f368d1ba547b4f5d03d46b98b3
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-07 19:10:24 -0400

    redo optimal algorithms

--- main.py
+++ main.py
@@ -9,30 +9,30 @@ you can't add a single integer to itself in order to obtain the target sum.
 You can assume that there will be at most one pair of numbers summing up to the target sum.
 """
 # Redone Algorithms for understanding
+# Soultion 1 with HashTable
 
-# Solution 1 - With a hashTable
 def addTwoSum(array, targetSum):
-    hashTable = {}
+    hashtable = {}
     for num in array:
-        if targetSum - num in hashTable:
+        if targetSum - num in hashtable:
             return [targetSum - num, num]
         else:
-            hashTable[num] = True
+            hashtable[num] = True
     return []
 
 
-# Solution 2 - Left/Right Pointers
-def addTwoSum2(array, targetSum):
+# Solution  2 with Left/Right Pointesr
+def addTwoSum(array, targetSum):
     array.sort()
     left = 0
     right = len(array) - 1
     while left != right:
         currentSum = array[left] + array[right]
-        if currentSum > targetSum:
-            right -= 1
+        if currentSum == targetSum:
+            return[array[left], array[right]]
         elif currentSum < targetSum:
             left += 1
         else:
-            return [array[left], array[right]]
+            right -= 1
     return []
 
